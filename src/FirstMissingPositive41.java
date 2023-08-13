02854cc5035a02873b6734f101bdee9ca02677a5
yuxin-dell-xps <yuxin_liu_1998@outlook.com>
2023-08-14 00:31:28 +0800

    add 77,78

---
+++ src/FirstMissingPositive41.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class FirstMissingPositive41 {
+
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for(int i = 0; i < n; i++) {
+            while(nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1])
+                swap(nums, i, nums[i] - 1);
+        }
+        for(int i = 0; i < n; i++)
+            if(nums[i] != i + 1)
+                return i + 1;
+        return n + 1;
+    }
+
+    public void swap(int []nums, int x,int y){
+        int temp = nums[x];
+        nums[x] = nums[y];
+        nums[y] = temp;
+    }
+
+    public static void main(String[] args) {
+        int [] nums = {99,94,96,11,92,5,91,89,57,85,66,63,84,81,79,61,74,78,77,30,64,13,58,18,70,69,51,12,32,34,9,43,39,8,1,38,49,27,21,45,47,44,53,52,48,19,50,59,3,40,31,82,23,56,37,41,16,28,22,33,65,42,54,20,29,25,10,26,4,60,67,83,62,71,24,35,72,55,75,0,2,46,15,80,6,36,14,73,76,86,88,7,17,87,68,90,95,93,97,98};
+        FirstMissingPositive41 sol = new FirstMissingPositive41();
+        System.out.println(sol.firstMissingPositive(nums));
+        System.out.println(Arrays.toString(nums));
+
+    }
+
+}
